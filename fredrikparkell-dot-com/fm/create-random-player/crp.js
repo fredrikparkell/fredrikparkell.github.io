49f8f15c1573dce26f99c2e4d12b08ae7efdcbd0
fredrikparkell <fredrikparkell5@gmail.com>
2021-08-10 09:08:29 +0200

    Now with random names
    Added files and functionality for random names

--- fredrikparkell-dot-com/fm/create-random-player/crp.js
+++ fredrikparkell-dot-com/fm/create-random-player/crp.js
@@ -42,43 +42,103 @@ function generatePlayer() {
 
     container.prepend(newPlayerTR);
 
-    let newPlayerCountry = document.createElement('td');
-    newPlayerCountry.innerText = randomCountry();
+    let newPlayerCountryElement = document.createElement('td');
+    newPlayerCountryElement.innerText = randomCountry();
 
-    let newPlayerPotential = document.createElement('td');
-    newPlayerPotential.innerText = randomInt(200, 135);
+    let newPlayerNameElement = document.createElement('td');
+    newPlayerNameElement.innerText = randomName(newPlayerCountryElement.innerText);
+    newPlayerNameElement.innerHTML = newPlayerNameElement.innerHTML.replace("<br>", "");
+    newPlayerNameElement.innerHTML = newPlayerNameElement.innerHTML.replace("<br>", "");
+    console.log(newPlayerNameElement.innerHTML);
+    console.log(newPlayerNameElement.innerText);
 
-    let newPlayerPositions = document.createElement('td');
-    newPlayerPositions.innerText = randomPositions();
+    let newPlayerPotentialElement = document.createElement('td');
+    newPlayerPotentialElement.innerText = randomInt(200, 135);
 
-    player_list.push([newPlayerCountry.innerText, newPlayerPotential.innerText, newPlayerPositions.innerText]);
+    let newPlayerPositionsElement = document.createElement('td');
+    newPlayerPositionsElement.innerText = randomPositions();
+
+    player_list.push([newPlayerNameElement.innerText, newPlayerCountryElement.innerText, newPlayerPotentialElement.innerText, newPlayerPositionsElement.innerText]);
     
-    newPlayerTR.appendChild(newPlayerCountry);
-    newPlayerTR.appendChild(newPlayerPotential);
-    newPlayerTR.appendChild(newPlayerPositions);
+    newPlayerTR.appendChild(newPlayerNameElement);
+    newPlayerTR.appendChild(newPlayerCountryElement);
+    newPlayerTR.appendChild(newPlayerPotentialElement);
+    newPlayerTR.appendChild(newPlayerPositionsElement);
+}
+
+function randomName(countryName){
+    let firstName = readTextFile("./names/" + countryName + "-firstnames.txt");
+    let lastName = readTextFile("./names/" + countryName + "-lastnames.txt");
+    console.log(firstName);
+    console.log(lastName);
+    console.log(firstName + " " + lastName);
+
+    return firstName + " " + lastName;
+}
+
+function readTextFile(file){
+    let name;
+    var txtFile = new XMLHttpRequest();
+    txtFile.open("GET", file, false);
+    txtFile.onreadystatechange = function ()
+    {
+        if(txtFile.readyState === 4)
+        {
+            if(txtFile.status === 200 || txtFile.status == 0)
+            {
+                var result = txtFile.responseText;
+                let arr = result.split("\n");
+                name = randomNamePicker(arr);
+                console.log(name);
+            }
+        }
+    }
+    txtFile.send(null);
+    return name;
+}
+
+function randomNamePicker(array){
+    let isTrue = false;
+    let name;
+    while (isTrue != true) {
+        name = array[randomInt(array.length-1,0)];
+        if (name != ""){
+            isTrue = true;
+        }
+    }
+    return name;
 }
 
 function randomCountry() {
-    //195 countries as of 2nd august 2021 in this list
-    let country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba",
-    "Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia",
-    "Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon",
-    "Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia",
-    "Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea",
-    "Estonia","Ethiopia","Faroe Islands","Fiji","Finland","France","Gabon","Gambia",
-    "Georgia","Germany","Ghana","Gibraltar","Greece","Grenada","Guam","Guatemala","Guinea","Guinea Bissau","Guyana","Haiti",
-    "Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan",
-    "Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania",
-    "Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco",
-    "Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","New Caledonia","New Zealand",
-    "Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal",
-    "Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Saudi Arabia","Senegal",
-    "Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts & Nevis","St Lucia",
-    "St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo",
-    "Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay",
-    "Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe", "North Korea"];
+    let country_list = ["Afghanistan","Albanien","Algeriet","Amerikanska Jungfruöarna","Amerikanska Samoa","Andorra",
+    "Angola","Anguilla","Antigua och Barbuda","Argentina","Armenien","Aruba",
+    "Australien","Azerbajdzjan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgien","Belize","Benin","Bermuda","Bhutan","Bolivia",
+    "Bonaire","Bosnien","Botswana","Brasilien","Brittiska Jungfruöarna","Brunei","Bulgarien","Burkina Faso","Burundi",
+    "Caymanöarna","Centralafrikanska republiken",
+    "Chile","Colombia","Cooköarna","Costa Rica","Curacao","Cypern",
+    "Danmark","Djibouti","Dominica","Dominikanska Republiken","Ecuador","Egypten","Ekvatorialguinea","El Salvador",
+    "Elfenbenskusten","England","Eritrea","Estland","Etiopien","Fiji","Filippinerna","Finland","Frankrike","Franska Guyana",
+    "Färöarna","Förenade Arabemiraten","Gabon","Gambia",
+    "Georgien","Ghana","Gibraltar","Grekland","Grenada","Guadeloupe","Guam","Guatemala","Guinea Bissau","Guinea","Guyana","Haiti","Holland",
+    "Honduras","Hong Kong","Indien","Indonesien","Irak","Iran","Irland","Island","Israel","Italien","Jamaica","Japan","Jemen",
+    "Jordanien","Kambodja","Kamerun","Kanada","Kap Verde","Kazakhstan","Kenya","Kina","Kinesiska Taipei","Kirgizistan",
+    "Kiribati","Komorerna","Kongo DR","Kongo","Kosovo","Kroatien","Kuba","Kuwait",
+    "Laos","Lesotho","Lettland","Libanon","Liberia","Libyen","Liechtenstein","Litauen",
+    "Luxemburg","Macao","Madagaskar","Malawi","Malaysia","Maldiverna","Mali","Malta","Marocko","Martinique",
+    "Mauretanien","Mauritius","Mayotte","Mexiko","Mikronesien","Mocambique","Moldavien","Mongoliet",
+    "Montenegro","Montserrat","Myanmar","Namibia","Nepal","Nicaragua","Niger","Nigeria",
+    "Nordirland","Nordkorea","Nordmakedonien","Norge","Norra Marianaöarna","Nya Kaledonien","Nya Zeeland","Oman",
+    "Pakistan","Palestina","Panama","Papua Nya Guinea","Paraguay","Peru","Polen","Portugal",
+    "Puerto Rico","Qatar","Reunion","Rumänien","Rwanda","Ryssland",
+    "Saint Barthelemy","Saint Kitts och Nevis","Saint Lucia","Saint Martin","Saint Pierre och Miquelon","Saint Vincent","Salomonöarna",
+    "San Marino","Sao Tome","Saudiarabien","Schweiz","Senegal","Serbien","Seychellerna",
+    "Sierra Leone","Singapore","Sint Maarten","Skottland","Slovakien","Slovenien",
+    "Somalia","Spanien","Sri Lanka","Sudan","Surinam","Sverige","Swaziland","Sydafrika","Sydkorea","Sydsudan",
+    "Syrien","Tadzjikistan","Tahiti","Tanzania","Tchad","Thailand","Tjeckien","Togo","Tonga","Trinidad och Tobago","Tunisien",
+    "Turkiet","Turkmenistan","Turks och Caicosöarna","Tuvalu","Tyskland","Uganda","Ukraina","Ungern","Uruguay","USA",
+    "Uzbekistan","Vanuatu","Venezuela","Vietnam","Västra Samoa","Wales","Wallis och Futunaöarna","Zambia","Zanzibar","Zimbabwe","Österrike","Östtimor"];
 
-    return country_list[randomInt(0,194)];
+    return country_list[randomInt(0,country_list.length-1)];
 }
 
 function randomInt(max, min) {
@@ -86,16 +146,16 @@ function randomInt(max, min) {
 }
 
 function randomPositions() {
-    let positions_list = ["GK", "RB", "RWB", "CB", "LWB", "LB", "CDM", "CM", "COM", "RM", "RW", "LM", "LW", "ST",
-    "GK", "RB", "RWB", "CB", "LWB", "LB", "CDM", "CM", "COM", "RM", "RW", "LM", "LW", "ST",
-    "GK", "RB", "RWB", "CB", "LWB", "LB", "CDM", "CM", "COM", "RM", "RW", "LM", "LW", "ST",
-    "GK", "RB", "RWB", "CB", "LWB", "LB", "CDM", "CM", "COM", "RM", "RW", "LM", "LW", "ST"];
+    let positions_list = ["MV", "HB", "HYB", "MB", "VYB", "VB", "CDM", "CM", "COM", "HM", "HY", "VM", "VY", "FWD",
+    "MV", "HB", "HYB", "MB", "VYB", "VB", "CDM", "CM", "COM", "HM", "HY", "VM", "VY", "FWD",
+    "MV", "HB", "HYB", "MB", "VYB", "VB", "CDM", "CM", "COM", "HM", "HY", "VM", "VY", "FWD",
+    "MV", "HB", "HYB", "MB", "VYB", "VB", "CDM", "CM", "COM", "HM", "HY", "VM", "VY", "FWD",];
 
-    return positions_list[randomInt(0,51)];
+    return positions_list[randomInt(0,positions_list.length-1)];
 }
 
 function saveToCSV() {
-    let csv = 'country,potential,positions\n';
+    let csv = 'name,country,potential,positions\n';
 
     player_list.forEach(function(row) {  
         csv += row.join(',');  
